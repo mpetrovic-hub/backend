a49a0d28be8f6d1ae4b4281a1b4beec5bf03d7d2
mpetrovic <mpetrovic@kiwimobile.de>
2026-04-13 15:57:44 +0200

    lp4-fr

---
+++ landing-pages/lp4-fr/integration.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'key' => 'lp4-fr',
+    'country' => 'FR',
+    'flow' => 'nth-fr-one-off',
+    'provider' => 'nth',
+    'documentation' => '/integrations/nth/fr/one-off/README.md',
+
+    'locale' => 'fr',
+    'service_type' => 'premium_sms',
+    'business_number' => '84072',
+    'keyword' => 'Jplay*',
+    'title' => 'LP4 France One-off',
+    'active' => true,
+
+    'backend_path' => '/lp/fr/myjoyplay4',
+    'dedicated_path' => '/',
+    'hostnames' => ['frlp4.joy-play.com'],
+    'service_key' => 'nth_fr_one_off_jplay',
+    'shortcode' => '84072',
+    'price_label' => '4,50 EUR / SMS + prix d\'un SMS',
+];
