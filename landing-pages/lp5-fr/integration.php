d9194e83d874c6bdb8c631fee9233b7513256bfb
mpetrovic <mpetrovic@kiwimobile.de>
2026-04-17 11:09:15 +0200

    'dedicated_path' => '/',

--- landing-pages/lp5-fr/integration.php
+++ landing-pages/lp5-fr/integration.php
@@ -17,6 +17,7 @@ return [
     'backend_path' => '/lp/fr/myjoyplay5',
     'dedicated_path' => '/',
     'hostnames' => ['frlp5.joy-play.com', 'your.joy-play.com'],
+    'dedicated_path' => '/',
     'service_key' => 'nth_fr_one_off_jplay',
     'shortcode' => '84072',
     'price_label' => '4,50 EUR / SMS + prix d\'un SMS',
